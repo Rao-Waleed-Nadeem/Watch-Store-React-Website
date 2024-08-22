38e4336b556ebe5aea87514bf5552b0a38176c8f
Rao Waleed <rao.waleed.nadeem@gmail.com>
2024-08-23 00:31:49 +0500

    modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -8,6 +7,22 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: "index.html",
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "index.html") {
+            return assetInfo.name;
+          }
+          const ext = path.extname(assetInfo.name);
+          return `assets/[name]${ext}`;
+        },
+      },
     },
   },
 });
