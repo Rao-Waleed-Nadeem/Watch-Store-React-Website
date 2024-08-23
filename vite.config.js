cf788f3d876a17f7b4f9953cc4be204d08601fca
Rao Waleed <rao.waleed.nadeem@gmail.com>
2024-08-24 04:54:16 +0500

    fix netlify issues

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Set this to your deployment path if different
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
